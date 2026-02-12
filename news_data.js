a30833dd614b1dc0097da205c66c8e0d5990e6fa
aisyatirrodiyah <aisyah123@gmail.com>
2026-02-12 16:37:50 +0700

    upload pertama profile company

---
+++ news_data.js
@@ -0,0 +1,38 @@
+﻿const newsData = [
+    {
+        "title":  "Peran Hakim Dalam Melindungi Hak-hak Anak Dalam Surat Edaran Mahkamah Agung Nomor 1 Tahun 2025",
+        "description":  "Klik detail untuk membaca berita selengkapnya dari website resmi Pengadilan Agama Pamekasan.",
+        "link":  "https://pa-pamekasan.go.id/article/Peran-Hakim-Dalam-Melindungi-Hak-hak-Anak-Dalam-Surat-Edaran-Mahkamah-Agung-Nomor-1-Tahun-2025",
+        "image":  "https://pa-pamekasan.go.id/arsip/images/20260119165844.jpg"
+    },
+    {
+        "title":  "Dinamika Kriminalisasi Perkawinan Berhalangan Dalam Pasal 402 Uu No. 1 Tahun 2023",
+        "description":  "Klik detail untuk membaca berita selengkapnya dari website resmi Pengadilan Agama Pamekasan.",
+        "link":  "https://pa-pamekasan.go.id/article/Dinamika-Kriminalisasi-Perkawinan-Berhalangan-Dalam-Pasal-402-Uu-No.-1-Tahun-2023",
+        "image":  "https://pa-pamekasan.go.id/arsip/images/20260119165715.jpg"
+    },
+    {
+        "title":  "Fenomena Walkaway Wife Syndrome Sebagai Penyebab Perceraian Yang Tidak Disadari",
+        "description":  "Klik detail untuk membaca berita selengkapnya dari website resmi Pengadilan Agama Pamekasan.",
+        "link":  "https://pa-pamekasan.go.id/article/Fenomena-Walkaway-Wife-Syndrome-Sebagai-Penyebab-Perceraian-Yang-Tidak-Disadari",
+        "image":  "https://pa-pamekasan.go.id/arsip/images/20251231160631.jpg"
+    },
+    {
+        "title":  "Pengaturan dan Praktik Penunjukan Hakam dalam Perkara Syiqaq di Pengadilan Agama",
+        "description":  "Klik detail untuk membaca berita selengkapnya dari website resmi Pengadilan Agama Pamekasan.",
+        "link":  "https://pa-pamekasan.go.id/article/Pengaturan-dan-Praktik-Penunjukan-Hakam-dalam-Perkara-Syiqaq-di-Pengadilan-Agama",
+        "image":  "https://pa-pamekasan.go.id/arsip/images/20251231160455.jpg"
+    },
+    {
+        "title":  "Dialektika Ilmu dan Amal. Fondasi Teologis dan Yuridis dalam Mewujudkan Keadilan",
+        "description":  "Klik detail untuk membaca berita selengkapnya dari website resmi Pengadilan Agama Pamekasan.",
+        "link":  "https://pa-pamekasan.go.id/article/Dialektika-Ilmu-dan-Amal.-Fondasi-Teologis-dan-Yuridis-dalam-Mewujudkan-Keadilan",
+        "image":  "https://pa-pamekasan.go.id/arsip/images/20251231160304.jpg"
+    },
+    {
+        "title":  "Optimalkan Kompetensi Mahasiswa Magang. Kasubag Umum dan Keuangan PA Pamekasan Berikan Pembekalan Materi",
+        "description":  "Klik detail untuk membaca berita selengkapnya dari website resmi Pengadilan Agama Pamekasan.",
+        "link":  "https://pa-pamekasan.go.id/Optimalkan-Kompetensi-Mahasiswa-Magang.-Kasubag-Umum-dan-Keuangan-PA-Pamekasan-Berikan-Pembekalan-Materi",
+        "image":  "assets/pa_pamekasan_official.jpg"
+    }
+];
